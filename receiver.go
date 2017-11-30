a53727345b47e671079adfd7b4bbfa7ee62bccef
gujarats <gujarat.santana@gmail.com>
2017-11-30 07:50:36 +0700

    receiver request into struct with test

---
+++ receiver.go
@@ -0,0 +1,97 @@
+package receiver
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+const tagKey = "request"
+
+// Set data value from http.Request.Form
+func SetData(data interface{}, r *http.Request) error {
+
+	dataValue := reflect.ValueOf(data)
+	if dataValue.Kind() != reflect.Ptr {
+		return errors.New("data argument must has address eg: &result")
+	}
+
+	form := r.Form
+	if len(form) == 0 {
+		return errors.New("request form is empty")
+	}
+
+	dataType := reflect.TypeOf(data)
+	for i := 0; i < dataValue.Elem().NumField(); i++ {
+		field := dataType.Elem().Field(i)
+		tagValue := field.Tag.Get(tagKey)
+		keyValue, validator := getRequirements(tagValue)
+
+		//getting value from request form
+		value := r.FormValue(keyValue)
+		if value == "" && isRequired(validator) {
+			return errors.New("required value form is empty on the key = " + keyValue)
+		}
+
+		// parse the value to the respected type field
+		fieldElement := dataValue.Elem().Field(i)
+		fieldAddr := fieldElement.Addr().Interface()
+		if value != "" {
+			switch fieldType := fieldAddr.(type) {
+			case *int64:
+				parseValue, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					return errors.New("Parse integer failed check your tag key " + keyValue)
+				}
+				*fieldType = parseValue
+			case *float64:
+				parseValue, err := strconv.ParseFloat(value, 64)
+				if err != nil {
+					return errors.New("Parse float failed check your tag key " + keyValue)
+				}
+				*fieldType = parseValue
+			case *string:
+				*fieldType = value
+			}
+		}
+
+	}
+
+	return nil
+}
+
+// check validator is required or not
+func isRequired(validator string) bool {
+	if validator == "required" {
+		return true
+	}
+
+	return false
+}
+
+// getting value [0] = value [1] = required/optional
+// which coming from data interface{}
+// return the key value and validator in sequence order
+func getRequirements(tagValue string) (string, string) {
+	values := strings.Split(tagValue, ",")
+	if len(values) == 2 {
+		return values[0], values[1]
+	}
+
+	return "", ""
+
+}
+
+func getAllTags(data interface{}, r *http.Request) []string {
+	var tagValues []string
+	t := reflect.TypeOf(data)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tagValue := field.Tag.Get(tagKey)
+		tagValues = append(tagValues, tagValue)
+	}
+
+	return tagValues
+}
---
+++ receiver_test.go
@@ -0,0 +1,164 @@
+package receiver
+
+import (
+	"errors"
+	"net/http"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+type testStructOk struct {
+	Lat      float64 `request:"latitude,required"`
+	Lon      float64 `request:"longitude,required"`
+	Name     string  `request:"name,required"`
+	Distance int64   `request:"distance,optional"`
+}
+
+type testStructNotOk struct {
+	Lat      float64 `request:"latitude,required"`
+	Lon      float64 `request:"longitude,required"`
+	Name     string  `request:"name,required"`
+	Distance int64   `request:"name,optional"`
+}
+
+func TestReceiver(t *testing.T) {
+	testObjects := []struct {
+		r           http.Request
+		data        *testStructOk
+		dataNotOk   *testStructNotOk
+		ok          bool
+		expectedErr error
+	}{
+		// test 0
+		{
+			r: http.Request{
+				Form: url.Values{
+					"latitude":  []string{"123.123456"},
+					"longitude": []string{"12.123456"},
+					"name":      []string{"gujarat"},
+					"distance":  []string{"1234"},
+				},
+			},
+			data: &testStructOk{},
+			ok:   true,
+		},
+
+		// test 1
+		{
+			r: http.Request{
+				Form: url.Values{
+					"latitude":  []string{"123.123456"},
+					"longitude": []string{"12.123456"},
+					"name":      []string{"gujarat"},
+					"distance":  []string{},
+				},
+			},
+			data: &testStructOk{},
+			ok:   true,
+		},
+
+		// test 2
+		{
+			r: http.Request{
+				Form: url.Values{
+					"latitude":  []string{"123.123456"},
+					"longitude": []string{"12.123456"},
+					"name":      []string{},
+					"distance":  []string{},
+				},
+			},
+			data:        &testStructOk{},
+			ok:          false,
+			expectedErr: errors.New("required value form is empty on the key = name"),
+		},
+
+		// test 3
+		{
+			r: http.Request{
+				Form: url.Values{
+					"latitude":  []string{},
+					"longitude": []string{"12.123456"},
+					"name":      []string{},
+					"distance":  []string{},
+				},
+			},
+			data:        &testStructOk{},
+			ok:          false,
+			expectedErr: errors.New("required value form is empty on the key = latitude"),
+		},
+
+		// test 4
+		{
+			r: http.Request{
+				Form: url.Values{
+					"latitude":  []string{"123.123456"},
+					"longitude": []string{"12.123456"},
+					"name":      []string{"gujarat"},
+					"distance":  []string{"1234"},
+				},
+			},
+			dataNotOk:   &testStructNotOk{},
+			ok:          false,
+			expectedErr: errors.New("Parse integer failed check your tag key name"),
+		},
+	}
+
+	for index, testObject := range testObjects {
+		if testObject.data != nil {
+			err := SetData(testObject.data, &testObject.r)
+			if err != nil && testObject.ok {
+				t.Errorf("%+v\n", err)
+			}
+
+			if isAnyFieldEmpty(testObject.data) && testObject.ok {
+				t.Errorf("Failed index = %v, struct = %+v is empty", index, testObject.data)
+			}
+
+			if !testObject.ok {
+				assert.Equal(t, testObject.expectedErr, err)
+			}
+		}
+
+		if testObject.dataNotOk != nil {
+			err := SetData(testObject.dataNotOk, &testObject.r)
+			if err != nil && testObject.ok {
+				t.Errorf("%+v\n", err)
+			}
+
+			if isAnyFieldEmpty(testObject.dataNotOk) && testObject.ok {
+				t.Errorf("Failed index = %v, struct = %+v is empty", index, testObject.data)
+			}
+
+			if !testObject.ok {
+				assert.Equal(t, testObject.expectedErr, err)
+			}
+
+		}
+	}
+}
+
+// passing struct and check all the field,
+// false if there is empty value.
+func isAnyFieldEmpty(input interface{}) bool {
+	object := reflect.ValueOf(input)
+
+	dataType := reflect.TypeOf(input)
+	for index := 0; index < object.Elem().NumField(); index++ {
+		field := dataType.Elem().Field(index)
+		tagValue := field.Tag.Get(tagKey)
+		_, validator := getRequirements(tagValue)
+		if isZeroOfUnderlyingType(object.Elem().Field(index).Interface()) && validator == "required" {
+			return true
+		}
+	}
+
+	return false
+}
+
+// compare the object value to non assign value.
+func isZeroOfUnderlyingType(objectValue interface{}) bool {
+	return reflect.DeepEqual(objectValue, reflect.Zero(reflect.TypeOf(objectValue)).Interface())
+}
